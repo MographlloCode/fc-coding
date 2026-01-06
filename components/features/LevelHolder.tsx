757c0193d0990c20c4e5ae6e47485a59edda01e3
Gustavo Mello <GMELLOCONTACT@GMAIL.COM>
2026-01-05 21:52:40 -0300

    feat: level holder component

--- components/features/LevelHolder.tsx
+++ components/features/LevelHolder.tsx
@@ -1,3 +1,5 @@
+import { CircularProgressor } from "../common/CircularProgressor";
+
 interface LevelHolderProps {
   level: number;
   percentage: number;
@@ -5,43 +7,11 @@ interface LevelHolderProps {
 }
 
 export function LevelHolder({ level, percentage, size = 80 }: LevelHolderProps) {
-  const strokeWidth = 3;
-  const radius = (size - strokeWidth) / 2;
-  const circumference = 2 * Math.PI * radius;
-  
-  const offset = circumference - (percentage / 100) * circumference;
-
   return (
-    <div className="relative flex items-center justify-center" style={{ width: size, height: size }}>
-      <svg className="transform -rotate-90" width={size} height={size}>
-        <circle
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          stroke="currentColor"
-          strokeWidth={strokeWidth}
-          fill="transparent"
-          className="text-zinc-800"
-        />
-        <circle
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          stroke="currentColor"
-          strokeWidth={strokeWidth}
-          fill="transparent"
-          strokeDasharray={circumference}
-          style={{ 
-            strokeDashoffset: offset,
-            transition: "stroke-dashoffset 0.5s ease-in-out" 
-          }}
-          strokeLinecap="round"
-          className="text-indigo-500"
-        />
-      </svg>
+    <CircularProgressor percentage={percentage} size={size} >
       <span className="absolute text-sm font-bold text-indigo-200">
         {level}
       </span>
-    </div>
+    </CircularProgressor>
   );
 }
